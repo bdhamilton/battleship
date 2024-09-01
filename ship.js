55dec0c7cbf65b0dfe57acc9752869aa05cd55fe
Brian Hamilton <bdhamilton@gmail.com>
2024-08-31 22:26:35 -0400

    Write Gameboard class

--- ship.js
+++ ship.js
@@ -9,8 +9,102 @@ class Ship {
   }
 
   isSunk() {
-    return this.hits >= this.length ? true : false;
+    return this.hits >= this.length;
   }
 }
 
-module.exports = Ship;
+class Gameboard {
+  constructor(width, height) {
+    // Create an array of arrays to represent the gameboard.
+    this.board = [];
+    for (let i = 0; i < height; i++) {
+      let row = [];
+      for (let j = 0; j < width; j++) {
+        row.push(null);
+      }
+      this.board.push(row);
+    }
+
+    this.width = width;
+    this.height = height;
+    this.ships = [];
+  }
+
+  placeShip(row, column, length, horizontal = true) {
+    // Can we place this ship here?
+    if (!this.canPlaceShip(row, column, length, horizontal)) {
+      throw new Error("Can't place this ship here.");
+    }
+
+    // Build a new ship and add it to the board.
+    const ship = new Ship(length);
+    this.ships.push(ship);
+
+    // Place it on the appropriate coordinates.
+    if (horizontal) {
+      // Add it to each column in the designated row...
+      for (let i = 0; i < length; i++) {
+        this.board[row][column + i] = ship;
+      }
+    } else {
+      // ...or to each row in the designated column.
+      for (let i = 0; i < length; i++) {
+        this.board[row + i][column] = ship;
+      }     
+    }
+  }
+
+  canPlaceShip(row, column, length, horizontal = true) {
+    // Is the ship too long to fit on the board?
+    if (horizontal) {
+      if (column + length > this.width) {
+        return false;
+      }
+    } else {
+      if (row + length > this.height) {
+        return false;
+      }
+    }
+
+    // Are there other ships already in the way?
+    if (horizontal) {
+      // Check `length` columns in the designated row.
+      for (let i = 0; i < length; i++) {
+        // If we run into anything, we can't place the ship.
+        if (this.board[row][column + i]) {
+          return false;
+        }
+      }
+    } else {
+      // Check `length` rows in the designated column.
+      for (let i = 0; i < length; i++) {
+        // If we run into anything, we can't place the ship.
+        if (this.board[row + i][column]) {
+          return false;
+        }
+      }     
+    }
+
+    // If we didn't find any conflicts, we can place the ship.
+    return true;
+  }
+
+  receiveAttack(row, column) {
+    const square = this.board[row][column];
+
+    if (square === null) {
+      this.board[row][column] = 'miss';
+      return 'miss';
+    } else if (typeof square === 'object') {
+      square.hit();
+      this.board[row][column] = 'hit';
+      return 'hit';
+    }
+  }
+
+  allSunk() {
+    return this.ships.every((ship) => ship.isSunk());
+  }
+}
+
+module.exports = { Ship, Gameboard };
--- ship.test.js
+++ ship.test.js
@@ -1,6 +1,7 @@
-const Ship = require('./ship.js');
+const { Ship, Gameboard } = require('./ship.js');
 
 test('creates a ship of a specified length', () => {
+
   expect(new Ship(5).length).toBe(5);
 });
 
@@ -20,4 +21,45 @@ test('ships can be sunk', () => {
   dingy.hit();
 
   expect(dingy.isSunk()).toBe(true);
-})
+});
+
+test('creates gameboard of correct size', () => {
+  expect(new Gameboard(3, 5).board.length).toBe(5);
+  expect(new Gameboard(3, 5).board[0].length).toBe(3);
+});
+
+test('recognizes when a ship does not fit', () => {
+  const game = new Gameboard(3, 5);
+  expect(game.canPlaceShip(0, 0, 25)).toBe(false);
+});
+
+test('places ships', () => {
+  const game = new Gameboard(3, 5);
+  game.placeShip(0, 0, 1);
+
+  expect(game.board[0][0]).not.toBeNull();
+  expect(() => game.placeShip(0, 0, 2)).toThrow(Error);
+});
+
+test('board can receive attacks', () => {
+  const game = new Gameboard(3, 5);
+  game.receiveAttack(0, 0);
+
+  expect(game.board[0][0]).not.toBeNull();
+});
+
+test('board can tell if all ships are sunk', () => {
+  const game = new Gameboard(3, 5);
+  game.placeShip(0, 0, 1);
+  game.receiveAttack(0, 0);
+
+  expect(game.allSunk()).toBe(true);
+  expect(game.board[0][0]).toBe('hit');
+});
+
+test('board knows if ships still remain afloat', () => {
+  const game = new Gameboard(3, 5);
+  game.placeShip(0, 0, 1);
+
+  expect(game.allSunk()).toBe(false);
+});
